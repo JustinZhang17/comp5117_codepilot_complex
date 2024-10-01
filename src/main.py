c3b52681e0c2a77cbf84976130a5ef1c12a8bafd
Justin Zhang <jing.justin@gmail.com>
2024-09-30 20:48:59 -0400

    add langchain libaries

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
-import uvicorn
+# import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+import uvicorn
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -12,3 +16,10 @@ async def Hello():
 def start():
     """Launched with `poetry run start` at root level"""
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+# API_KEY = os.getenv("LANGCHAIN_API_KEY")
+# TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+#
+# print(f"API Key: {API_KEY}")
+# print(f"Tracing V2: {TRACING_V2}")
